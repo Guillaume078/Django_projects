c9b3a043d2d996e74cad9b46c98ca2fb6e32becd
Guillaume <guillaumeilboudo078@gmail.com>
2023-08-31 19:09:16 +0100

    Tutoriel realpython 'Autorisation daffichage'.

---
+++ Autorisation d'affichage/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\USER PC\Documents\Django\Autorisation d'affichage\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
